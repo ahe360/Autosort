7f311e23198ea745ac4c9ec51df7b1e31c70ad03
Anthony He <anthonyhe@Anthonys-MacBook-Pro-4.local>
2024-06-26 14:26:37 -0700

    Initial commit of Chrome extension files

---
+++ background.js
@@ -0,0 +1,39 @@
+const imageExtensions = ['.jpg', '.jpeg', '.jpe', '.jif', '.jfif', '.jfi', '.png', '.gif', '.webp',
+                        '.tiff', '.tif', '.psd', '.raw', '.arw', '.cr2', '.nrw', '.k25', '.bmp', 
+                        '.dib', '.heif', '.heic', '.ind', '.indd', '.indt', '.jp2', '.j2k', '.jpf',
+                        '.jpf', '.jpx', '.jpm', '.mj2', '.svg', '.svgz', '.ai', '.eps', '.ico'];
+
+const videoExtensions = ['.webm', '.mpg', '.mp2', '.mpeg', '.mpe', '.mpv', '.ogg', '.mp4', '.mp4v',
+                        '.m4v', '.avi', '.wmv', '.mov', '.qt', '.flv', '.swf', '.avchd'];
+
+const audioExtensions = ['.m4a', '.flac', 'mp3', '.wav', '.wma', '.aac'];
+
+const documentExtensions = ['.doc', '.docx', '.odt', '.pdf', '.xls', '.xlsx', '.ppt', '.pptx'];
+
+// Listener for downloads
+chrome.downloads.onCreated.addListener(function(downloadedItem) {
+    chrome.storage.sync.get(['images', 'videos', 'music', 'documents'], function(items) {
+        const fileName = downloadItem.filename.toLowerCase();
+        let destination = '';
+
+        // Checks if the end of a file name matches with any of the specified extensions 
+        if (imageExtensions.some(ext => fileName.endsWith(ext))) {
+            destination = items.images || '';
+        } else if (videoExtensions.some(ext => fileName.endsWith(ext))) {
+            destination = items.videos || '';
+        } else if (audioExtensions.some(ext => fileName.endsWith(ext))) {
+            destination = items.music || '';
+        } else if (documentExtensions.some(ext => fileName.endsWith(ext))) {
+            destination = items.documents || '';
+        } 
+
+        if (destination) {
+            const newPath = '${destination}\\${downloadedItem.filename}';
+            chrome.downloads.onDeterminingFileName.addListener(function(item, suggest) {
+                if (item.id === downloadItem.id) {
+                    suggest({ filename: newPath });
+                }
+            });
+        }
+    });
+});
---
+++ popup.js
@@ -0,0 +1,33 @@
+document.addEventListener('DOMContentLoaded', function() {
+    loadOptions();
+});
+
+// Function to load saved options from Chrome's storage and populate input fields
+function loadOptions() {
+    chrome.storage.sync.get(['images', 'videos', 'music', 'documents'], function(items) {
+        // Set input fields to be retrieved values or empty strings if not yet defined by user
+        document.getElementById('images').value = items.images || '';
+        document.getElementById('videos').value = items.videos || '';
+        document.getElementById('music').value = items.music || '';
+        document.getElementById('documents').value = items.documents || '';
+    });
+}
+
+// Function to save user-input values from input fields into Chrome's storage
+function saveOptions() {
+    // Retrieve values from input fields; key-value pairs with the value being user's input paths
+    const images = document.getElementById('images').value
+    const videos = document.getElementById('videos').value;
+    const music = document.getElementById('music').value;
+    const documents = document.getElementById('documents').value;
+
+    // Save values in Chrome's storage
+    chrome.storage.sync.set({
+        images: images,
+        videos: videos,
+        music: music,
+        documents: documents
+    }, function() {
+        alert('Options saved.');
+    });
+}
