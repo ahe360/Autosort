fbd2da635a60718c0e8915c2a70e78647acf2140
ANTHONY HE <s-ANHE@lwsd.org>
2024-06-28 21:42:50 -0700

    Added logging statement for background script

--- background.js
+++ background.js
@@ -1,3 +1,5 @@
+console.log('Service worker is running');
+
 const imageExtensions = ['.jpg', '.jpeg', '.jpe', '.jif', '.jfif', '.jfi', '.png', '.gif', '.webp',
                         '.tiff', '.tif', '.psd', '.raw', '.arw', '.cr2', '.nrw', '.k25', '.bmp', 
                         '.dib', '.heif', '.heic', '.ind', '.indd', '.indt', '.jp2', '.j2k', '.jpf',
