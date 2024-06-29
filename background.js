1afdeec1c839fe12e25ff09941d8e074f9b6ce87
ahe360 <anthonyhe360@gmail.com>
2024-06-29 12:09:15 -0700

    Added listener for download

--- background.js
+++ background.js
@@ -1,42 +1,63 @@
+console.log('Service worker is running');
 chrome.runtime.onInstalled.addListener(function() {
-    console.log('Service worker is running.');
-  });
-  
-  const imageExtensions = ['.jpeg', '.jpg', '.gif', '.png', '.svg', '.webp', '.tiff', '.tif', '.psd', '.raw', '.arw', '.cr2', '.nrw', '.k25', '.bmp', '.dib', '.heif', '.heic'];
-  const videoExtensions = ['.mp4', '.mkv', '.webm', '.flv', '.vob', '.ogv', '.ogg', '.drc', '.gifv', '.mng', '.avi', '.mts', '.m2ts', '.ts', '.mov', '.qt', '.wmv', '.yuv', '.rm', '.rmvb', '.asf', '.amv', '.mpg', '.mp2', '.mpeg', '.mpe', '.mpv', '.svi', '.3gp', '.3g2', '.mxf', '.roq', '.nsv', '.f4v', '.f4p', '.f4a', '.f4b'];
-  const documentExtensions = ['.doc', '.docx', '.xls', '.xlsx', '.ppt', '.pptx', '.pdf', '.odt', '.ods', '.odp', '.rtf', '.txt', '.wpd', '.tex', '.epub'];
-  const audioExtensions = ['.mp3', '.wav', '.aac', '.flac', '.ogg', '.wma', '.m4a', '.aiff', '.alac'];
-  
-  function extractFilename(url) {
-    let urlParts = url.split('/');
-    const filenameWithParams = urlParts[urlParts.length - 1];
-    return filenameWithParams.split('?')[0];
-  }
-  
-  function determineNewPath(filename, items) {
+    console.log("Service worker installed");
+});
+
+const imageExtensions = ['.jpg', '.jpeg', '.jpe', '.jif', '.jfif', '.jfi', '.png', '.gif', '.webp', '.tiff', '.tif', '.psd', '.raw', '.arw', '.cr2', '.nrw', '.k25', '.bmp', '.dib', '.heif', '.heic', '.ind', '.indd', '.indt', '.jp2', '.j2k', '.jpf', '.jpx', '.jpm', '.mj2', '.svg', '.svgz', '.ai', '.eps', '.ico'];
+const videoExtensions = ['.webm', '.mpg', '.mp2', '.mpeg', '.mpe', '.mpv', '.ogg', '.mp4', '.mp4v', '.m4v', '.avi', '.wmv', '.mov', '.qt', '.flv', '.swf', '.avchd'];
+const audioExtensions = ['.m4a', '.flac', 'mp3', '.wav', '.wma', '.aac'];
+const documentExtensions = ['.doc', '.docx', '.odt', '.pdf', '.xls', '.xlsx', '.ppt', '.pptx'];
+
+function extractFileName(url) {
+    const urlParts = url.split('/');
+    const fileNameWithParams = urlParts[urlParts.length - 1];
+    return fileNameWithParams.split('?')[0].split('&')[0];
+}
+
+function determineNewPath(fileName, items) {
     let destination = '';
-    
-    if (imageExtensions.some(ext => filename.endsWith(ext))) {
-      destination = items.images || '';
-    } else if (videoExtensions.some(ext => filename.endsWith(ext))) {
-      destination = items.videos || '';
-    } else if (documentExtensions.some(ext => filename.endsWith(ext))) {
-      destination = items.documents || '';
-    } else if (audioExtensions.some(ext => filename.endsWith(ext))) {
-      destination = items.music || '';
+
+    // Check which extension end of file matches with
+    if (imageExtensions.some(ext => fileName.endsWith(ext))) {
+        destination = items.images || '';
+        console.log('Image detected:', fileName, 'Destination:', destination);
+    } else if (videoExtensions.some(ext => fileName.endsWith(ext))) {
+        destination = items.videos || '';
+        console.log('Video detected:', fileName, 'Destination:', destination);
+    } else if (audioExtensions.some(ext => fileName.endsWith(ext))) {
+        destination = items.music || '';
+        console.log('Audio detected:', fileName, 'Destination:', destination);
+    } else if (documentExtensions.some(ext => fileName.endsWith(ext))) {
+        destination = items.documents || '';
+        console.log('Document detected:', fileName, 'Destination:', destination);
+    } else {
+        console.log('No matching extension found for:', fileName);
     }
-    
-    console.log(`Destination: ${destination}`);
-    return destination ? `${destination}\\${filename}` : '';
-  }
-  
-  chrome.downloads.onDeterminingFilename.addListener(function(item, suggest) {
+
+    return destination ? `${destination}\\${fileName}` : '';
+}
+
+// Add the listener outside of the onCreated handler
+chrome.downloads.onDeterminingFilename.addListener(function onDeterminingFilename(item, suggest) {
+    console.log('Determining filename for item:', item);
+    console.log('downloadItem:', item);
     chrome.storage.sync.get(['images', 'videos', 'music', 'documents'], function(items) {
-        const cleanFilename = extractFilename(item.url);
-        const newPath = determineNewPath(cleanFilename, items);
-      
+        const fileName = extractFileName(item.finalUrl || item.url);
+        console.log('File name with params:', fileName);
+        const cleanFileName = extractFileName(fileName);
+        console.log('Clean file name:', cleanFileName);
+        const newPath = determineNewPath(cleanFileName, items);
         if (newPath) {
-            suggest({filename: newPath});
+            console.log('New path:', newPath);
+            suggest({ filename: newPath });
+            console.log('Filename suggestion:', newPath);
+            chrome.downloads.onDeterminingFilename.removeListener(onDeterminingFilename);
         }
     });
-});  
+});
+
+// Listener for downloads
+chrome.downloads.onCreated.addListener(function(downloadItem) {
+    console.log('Download created:', downloadItem);
+    // This part is now moved to the onDeterminingFilename listener
+});
